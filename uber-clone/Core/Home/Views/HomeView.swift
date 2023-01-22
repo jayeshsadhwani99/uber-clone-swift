9ed07bd1a4d8513115d7cf1ca540905c4d3724bd
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-22 14:53:26 +0530

    button and input on home

---
+++ uber-clone/Core/Home/Views/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  uber-clone
+//
+//  Created by Jayesh Sadhwani on 22/01/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            LocationSearchActivationView()
+                .padding(.top, 72)
+            
+            MapViewActionButton()
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
