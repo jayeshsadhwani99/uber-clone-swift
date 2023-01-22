c85bf21b20ac2a70a46a61b7d9f246618d4a29a7
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-22 19:08:30 +0530

    search for locations

---
+++ uber-clone/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  LocationSearchViewModel.swift
+//  uber-clone
+//
+//  Created by Jayesh Sadhwani on 22/01/23.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    // MARK: - Properties
+    
+    @Published var results = [MKLocalSearchCompletion]()
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+}
+
+    // MARK: - MKLocalSearchCompleterDelegate
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
