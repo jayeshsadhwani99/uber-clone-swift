25729fe82a755cd913604e52db65b332521ff4a2
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-22 11:49:26 +0530

    Initial Commit

---
+++ uber-clone/uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_cloneApp.swift
+//  uber-clone
+//
+//  Created by Jayesh Sadhwani on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
