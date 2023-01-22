12a96732435d774c9e6a6668dd3fbe1a6cde0020
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-22 15:35:45 +0530

    search location ui

---
+++ uber-clone/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,67 @@
+//
+//  LocationSearchView.swift
+//  uber-clone
+//
+//  Created by Jayesh Sadhwani on 22/01/23.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    @State private var startLocationText = ""
+    @State private var destinationLocationText = ""
+    
+    var body: some View {
+        VStack {
+            // header view
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Circle()
+                        .fill(Color.black)
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack {
+                    TextField("Current Location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where To?", text: $destinationLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 64)
+            
+            Divider()
+                .padding(.vertical)
+            
+            // list view
+            ScrollView {
+                VStack (alignment: .leading) {
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        LocationSearchResultCell()
+                    }
+                }
+            }
+        }
+        .background(.white)
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
