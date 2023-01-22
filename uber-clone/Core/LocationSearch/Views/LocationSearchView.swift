3869ca1540c09c03621082fa9f328b44efa253ff
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-22 23:19:43 +0530

    animate the ride select

--- uber-clone/Core/LocationSearch/Views/LocationSearchView.swift
+++ uber-clone/Core/LocationSearch/Views/LocationSearchView.swift
@@ -54,8 +54,10 @@ struct LocationSearchView: View {
                     ForEach(viewModel.results, id: \.self) { result in
                         LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
                             .onTapGesture {
-                                viewModel.selectLocation(result)
-                                mapState = .locationSelected
+                                withAnimation(.spring()) {
+                                    viewModel.selectLocation(result)
+                                    mapState = .locationSelected
+                                }
                             }
                     }
                 }
